787e5c46a70980584a0cf34c7874ab29cd3639db
Florian Goussin <floriangoussin@floriangoussin.local>
2023-11-29 17:23:31 -0700

    fix some small issues

--- fgc-book-review.php
+++ fgc-book-review.php
@@ -2,10 +2,12 @@
 /**
  * Plugin Name:       Book Review
  * Description:       Book Review Block
- * Requires PHP:      7.4
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            Florian Goussin - FGC
- * Text Domain:       fgc_book_review
+ * License:           MIT
+ * Text Domain:       fgc-book-review
  *
  * @package           fgc-blocks
  */
